cf64e8b0062bf16f85b7db9403ac13b8a28c2fe8
mrudulkolambe <mrudulkolambe02@gmail.com>
2024-04-14 15:55:53 +0530

    changes

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -6,44 +6,44 @@ const PrivacyPolicy = () => {
 			<h1 className='text-4xl font-extrabold mb-8 text-center'>Privacy-Policy</h1>
 			<div>
 				<h2 className='font-bold text-2xl'>1. Information We Collect</h2>
-				<div className='flex flex-col gap-1  px-5'>
-					<h4 className='font-bold'>1.1 Personal  Information :-</h4>
-					<p className='px-5'>When you use the App, we may collect certain personal information from you, such as your name and email address through Google Authentication. This information is used to create and authenticate your account.</p>
-					<h4 className='font-bold'>1.2 Usage Data :-</h4>
-					<p className='px-5'>We may also collect information about how you use the App, including your interactions with the App's features and functionalities. This data helps us improve the App and provide a better user experience.</p>
+				<div className='container-main flex flex-col gap-1  px-5'>
+					<h4 className='font-bold'>1.1 Email Address :-</h4>
+					<p className='px-5'> <span className='list mr-3'></span> We collect your email address during the signup process to create and manage your account. Your email address may also be used to communicate important updates, promotions, or other relevant information.</p>
+					<h4 className='font-bold'>1.2 Phone Number :-</h4>
+					<p className='px-5'> <span className='list mr-3'></span> We require your phone number to facilitate communication with you regarding your transactions, orders, and any other necessary customer service inquiries.</p>
+					<h4 className='font-bold'>1.3 Address :-</h4>
+					<p className='px-5'> <span className='list mr-3'></span>  Your address is collected to ensure the successful delivery of services, including product shipments, pickups, or other related activities.</p>
+					<h4 className='font-bold'>1.4 Photo and Camera Access :-</h4>
+					<p className='px-5'> <span className='list mr-3'></span> We may request access to your device's camera for verification purposes. This may include capturing images of items you wish to sell or for identity verification.</p>
 				</div>
 			</div>
 			<div>
-				<h2 className='font-bold text-2xl'>2. How  We Use Your Information</h2>
-				<div className='flex flex-col gap-1  px-5'>
-					<h4 className='font-bold'>2.1 Providing and Improving the App :-</h4>
-					<p className='px-5'>We use the collected information to provide and enhance the App's features, functionalities, and user experience. This includes personalizing content, optimizing performance, and addressing technical issues.</p>
-					<h4 className='font-bold'>2.2 Communication:-</h4>
-					<p className='px-5'>We may use your email address to communicate with you about important updates, changes to the App, and other relevant information.</p>
-					<h4 className='font-bold'>2.3 Legal Requirements:-</h4>
-					<p className='px-5'>We may use or disclose your personal information if required to do so by law or in response to valid legal requests from authorized government authorities.</p>
+				<h2 className='font-bold text-2xl'>2. How We Use Your Information</h2>
+				<div className='container-main flex flex-col gap-1  px-5'>
+					<p className='px-5'> <span className='list mr-3'></span> We use the information collected to provide, maintain, and improve our services to you.</p>
+					<p className='px-5'> <span className='list mr-3'></span> Your email address may be used to send you important notifications, updates, and marketing communications related to Chintamani OldGold services. You can opt out of marketing communications at any time.</p>
+					<p className='px-5'> <span className='list mr-3'></span> Phone numbers are used for communication purposes regarding your transactions, orders, or any other customer service inquiries.</p>
+					<p className='px-5'> <span className='list mr-3'></span> Address information is utilized for the delivery of services, including product shipments or pickups.</p>
+					<p className='px-5'> <span className='list mr-3'></span> Photos and camera access are utilized for verification purposes, ensuring the security and integrity of transactions and user accounts.</p>
 				</div>
 			</div>
-			<div>
-				<h2 className='font-bold text-2xl'>3. Data Security</h2>
-				<p className='px-5'>We take data security seriously and implement reasonable measures to protect your personal information from unauthorized access, alteration, disclosure, or destruction.</p>
+			<div className='container-main'>
+				<h2 className=' font-bold text-2xl'>3. Information Sharing</h2>
+				<p className='px-5'> <span className='list mr-3'></span> We do not sell, trade, or otherwise transfer your personal information to third parties without your consent, except as described in this Privacy Policy.</p>
+				<p className='px-5'> <span className='list mr-3'></span> Your information may be shared with trusted third-party service providers who assist us in operating our website, conducting our business, or servicing you.</p>
+				<p className='px-5'> <span className='list mr-3'></span> We may also disclose your information when we believe release is appropriate to comply with the law, enforce our site policies, or protect ours or others' rights, property, or safety.</p>
 			</div>
-			<div>
-				<h2 className='font-bold text-2xl'>4. Third-Party Services</h2>
-				<p className='px-5'>We use Firebase, a third-party service provided by Google, to store and process data. Please review Google's Privacy Policy <a className='text-brown underline underline-offset-2' href="https://policies.google.com/privacy">https://policies.google.com/privacy</a> to understand how they handle your information.</p>
+			<div className='container-main'>
+				<h2 className='font-bold text-2xl'>4. Data Security</h2>
+				<p className='px-5'> <span className='list mr-3'></span> We implement a variety of security measures to maintain the safety of your personal information when you access our services.</p>
+				<p className='px-5'> <span className='list mr-3'></span> Your personal information is stored on secure servers and protected through encryption and other appropriate security measures.</p>
 			</div>
-			<div>
-				<h2 className='font-bold text-2xl'>5. Your Choices</h2>
-				<p className='px-5'>You can access, update, or delete your personal  information by contacting us at <a className='text-brown underline underline-offset-2' href="mailto:query.chintamanijewellers@gmail.com">query.chintamanijewellers@gmail.com</a>. You can also disable cookies or adjust your device settings to prevent the collection of certain information.</p>
+			<div className='container-main'>
+				<h2 className='font-bold text-2xl'>5. User Control</h2>
+				<p className='px-5'> <span className='list mr-3'></span> You have the right to review and update your personal information at any time through the app's settings or by contacting our customer support team.</p>
+				<p className='px-5'> <span className='list mr-3'></span> You can choose to opt out of receiving marketing communications from us by adjusting your notification preferences.</p>
 			</div>
-			<div>
-				<h2 className='font-bold text-2xl'>6. Changes to Privacy Policy</h2>
-				<p className='px-5'>We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. We encourage you to review this Privacy Policy periodically.</p>
-			</div>
-			<div>
-				<h2 className='font-bold text-2xl'>7. Contact Us</h2>
-				<p className='px-5'>If you have any questions, concerns, or requests regarding this Privacy Policy or your personal information, please contact us query <a className='text-brown underline underline-offset-2' href="mailto:chintamanijewellers@gmail.com">chintamanijewellers@gmail.com</a> By using the Chintamani  Old Gold  App, you agree to the terms outlined in this Privacy Policy. If you do not agree with any aspect of this Policy, please do not use the App.</p>
-			</div>
+			<p className='px-5'> <span className='list mr-3'></span> By using the Sell on Chintamani OldGold App, you consent to the terms outlined in this Privacy Policy. We reserve the right to modify this Privacy Policy at any time, so please review it frequently. If you have any questions or concerns regarding our Privacy Policy, please contact us at [<a className='underline' href='tel:+918097872030'>+918097872030</a>]</p>
 		</main>
 	)
 }
